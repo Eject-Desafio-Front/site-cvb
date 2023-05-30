17c446366e93c94a4e1d26b3c8ec01481be51710
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-05-30 18:10:58 -0300

    feat: Cria template básico de html, css e javascript

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById('btn');
+
+// Adicionando um manipulador de evento para o clique do botão
+botao.addEventListener('click', function () {
+    // Ação a ser executada quando o botão for clicado
+    alert('O botão foi clicado!');
+});
